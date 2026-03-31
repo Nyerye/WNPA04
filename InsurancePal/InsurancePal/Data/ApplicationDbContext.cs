550a8b662921bc2fb9c25a79e87c6ad66e091037
Nicholas Reilly <90353424+Nyerye@users.noreply.github.com>
2026-03-30 21:52:18 -0400

    -Created the ApplicationDbContext class that inheirts from the Entity Framework;s DbContext class. This allwos it to get the functionality fot ables, querying, etc.
    -Made the classes constructor that takes in the options as a overload.
    
    -Creating a public DbSet for the Item class I made earlier so the database can make an Items table.

---
+++ InsurancePal/InsurancePal/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using InsurancePal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsurancePal.Data
+{
+    /// <summary>
+    /// ApplicationDbContext class that inherits from the DbContext class
+    /// </summary>
+    public class ApplicationDbContext : DbContext
+    {
+        //Constructor for the ApplicationDbContext class that takes in DbContextOptions and passes it to the base class constructor.
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //DbSet property for the Item model, which allows for querying and saving instances of the Item class.
+        //Allows for the database to make an Items table.
+        public DbSet<Item> Items { get; set; }
+    }
+}
---
+++ InsurancePal/InsurancePal/Models/Item.cs
@@ -0,0 +1,134 @@
+﻿/// <file>
+/// Item.cs 
+/// </file>
+/// <project>
+/// Windows Network Programming Assignment 4 
+/// </project>
+/// <author>
+/// Nicholas Reilly
+/// </author>
+/// <date>
+/// March 28 2026
+/// </date>
+/// <description>
+/// 
+/// </description>
+/// <references>
+/// Deitel, P., & Deitel, H. (2017). *C# 6 for Programmers Sixth Edition* 
+/// (Sixth, Ser. Deitel Development Series). Pearson Education.
+/// </references>
+/// 
+namespace InsurancePal.Models
+{
+    public class Item
+    {
+        private int itemid;
+        private string name;
+        private string category;
+        private string room;
+        private decimal estimatedvalue;
+        private DateTime purchasedate;
+        private string description;
+
+        public int ItemId
+        {
+            get
+            {
+                return itemid;
+            }
+            set
+            {
+                itemid = value;
+            }
+
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+
+            set
+            {
+                category = value;
+            }
+        }
+
+        public string Room
+        {
+            get
+            {
+                return room;
+            }
+
+            set
+            {
+                room = value;
+            }
+        }
+
+        public decimal EstimatedValue
+        {
+            get
+            {
+                return estimatedvalue;
+            }
+            set
+            {
+                estimatedvalue = value;
+            }
+        }
+
+        public DateTime purchasedDate
+        {
+            get
+            {
+                return purchasedate;
+
+            }
+
+            set
+            {
+                purchasedate = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+      public Item (int itemid, string name, string category, string room, decimal estimatedvalue, DateTime purchasedate, string description)
+        {
+            this.itemid = itemid;
+            this.name = name;
+            this.category = category;
+            this.room = room;
+            this.estimatedvalue = estimatedvalue;
+            this.purchasedate = purchasedate;
+            this.description = description;
+        }
+    }
+}
