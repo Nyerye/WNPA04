e5cc49822a35b027a1e58f0579560a6d0d8e9864
Nicholas Reilly <90353424+Nyerye@users.noreply.github.com>
2026-03-31 22:31:35 -0400

    -Created the Item controller that has all the assocaited CRUD operations with it.
    -Added the display annotation that allows the site to render the names of the columns proeprly and not by what they are referenced by in the code.

--- InsurancePal/InsurancePal/Models/Item.cs
+++ InsurancePal/InsurancePal/Models/Item.cs
@@ -86,6 +86,7 @@ namespace InsurancePal.Models
             }
         }
 
+        [Display(Name = "Estimated Value")]
         [Column(TypeName = "decimal(10,2)")]
         public decimal EstimatedValue
         {
@@ -99,6 +100,7 @@ namespace InsurancePal.Models
             }
         }
 
+        [Display(Name = "Purchased Date")]
         [DataType(DataType.Date)]
         public DateTime purchasedDate
         {
